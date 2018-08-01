7c470d0336611c6189b8b696bb90c6aa3f8ce68d
stanley06 <stanleyli0602@gmail.com>
2018-08-01 18:38:06 -0400

    added steps to config passwordless ssh

--- zsh_local/zshrc_local_after.zsh
+++ zsh_local/zshrc_local_after.zsh
@@ -1,10 +1,19 @@
-alias ll='ls -ahl'
-alias rm='rm -i'
+#alias ll='ls -ahl'
 
 #function alias
-alias g='/usr/local/bin/git'
-alias vim='/usr/local/bin/vim'
-alias ssh='TERM=xterm ssh'
 #JAVA_HOME='' # setup java home
+
+# pin to specific version
+#alias g='/usr/local/bin/git'
+#alias v='/usr/local/bin/vim'
+
+# company setup
+
+# 1) passwordless ssh setup
+# ssh-keygen # generate a key pair, name it test
+# ssh-copy-id -i .ssh/test.pub name@domain # append test.pub key into remote .ssh/authorized_keys
+# ssh-add .ssh/test # make sure the private key is added to the ssh agent
+
+alias dssh='ssh sli02@sli02.bf2.tumblr.net'
 
 echo "zsh_local_after run"
